9edb1072e66852f828559a5dd23b6472e6b8afe0
viniman <vinicius.carlos@live.com>
2019-05-20 21:04:13 -0300

    Resolucao de problemas, funcoes de ordem superior, tipos def pelo programador, criacao de Program.hs

--- haskellProgram.hs
+++ haskellProgram.hs
@@ -254,7 +254,7 @@ metade x = x / 2
 -- Dobrar uma lista de elementos
 dobrar :: Num a => [a] -> [a] -- Num: tanto faz se eh inteiro ou real
 dobrar [] = []
-dobrar (x:xs) = (x*2) : dobro xs
+dobrar (x:xs) = (x*2) : dobrar xs
 
 -- Incrementar os elementos de uma lista
 incrementar :: [Int] -> [Int]
@@ -281,12 +281,12 @@ map_myself f (x:xs) = f x : map f xs
 -- a funcao eh o operador * e o argumento dela eh 2
 -- vai aplica em cada elemento da lista 1 -> que eh igual a (x:xs)
 dobrar_map :: Num a => [a] -> [a]
-dobrar_map 1 = map (*2) 1
+dobrar_map l = map (*2) l
 
 -- pŕimeiro argumento: (+1)
 -- segundo argumento: 1
 incrementar_map :: [Int] -> [Int]
-incrementar_map 1 = map (+1) 1
+incrementar_map l = map (+1) l
 
 
 -- Exemplos aplicaveis diretamente no terminal
@@ -308,7 +308,7 @@ incrementar_map 1 = map (+1) 1
 -- separar elementos: os que passam pelo filtro eh os que quero preservar
 pega_letras :: String -> String
 pega_letras [] = []
-pega_letras
+pega_letras (x:xs)
 	| isAlpha x = x : pega_letras xs
 	| otherwise = pega_letras xs
 
@@ -340,3 +340,135 @@ pega_digitos' xs = filter isDigit xs
 -- filter (even) [1..10] - even: par ou impar (True para par)
 -- filter (/='a') "haskell"
 
+
+
+-- Reducao
+
+-- transformar todos os elementos de uma lista em um unico valor
+-- propriedade (funcao) de transformacao
+-- Exemplo: somatorio
+
+somatorio :: Num a => [a] -> a
+somatorio [] = 0
+somatorio (x:xs) = x + somatorio xs
+
+ou_logico :: [Bool] -> Bool
+ou_logico [] = False
+ou_logico (x:xs) = x || ou_logico xs
+
+
+-- f [] = z
+-- f (x:xs) = x # f xs
+-- #: qualquer operador
+-- f transforma lista vazia em z
+-- lista nao-vazia (x:xs) usando operacao # para combinar x com o resultado de f xs
+
+-- A funcao de ordem superior "foldr" abstrai o padrao de recursao
+-- argumentos: funcao (operacao) # e o valor do caso base z
+
+
+-- possivel implementacao de foldr:
+foldr' :: (a -> b -> b) -> b -> [a] ->b
+foldr' f z [] = z
+foldr' f z (x:xs) = f x (foldr f z xs)
+
+somatorio_foldr :: Num a => [a] -> a
+somatorio_foldr ls = foldr (+) 0 ls
+
+ou_logico_foldr :: [Bool] -> Bool
+ou_logico_foldr ls = foldr (||) False ls
+-- ls: lista de entrada
+-- False: caso base
+
+
+-- visualizando o foldr
+-- somatorio [1,2,3,4]
+-- foldr (+) 0 [1,2,3,4]
+-- foldr (+) 0 (1:(2:(3:(4:[]))))
+-- convertendo [] no caso-base
+-- foldr (+) 0 (1:(2:(3:(4:0))))
+-- convertendo : em +
+-- 1+(2+(3+(4+0)))
+
+
+-- Principais funcoes de ordem superior:
+-- map: modificar todos elementos da lista
+-- filter: separa os elementos da lista
+-- foldr: combina os elementos de uma lista
+
+----------------------------------------------------------------
+-- Exemplos: Mais sobre funcoes de ordem superior
+
+-- Defina uma funcaoo que considera um inteiro n como argumento e 
+-- devolve a soma dos quadrados dos n primeiros inteiros
+-- Combinar map e foldr:
+-- map: elevar lista ao quadrado
+-- foldr: somar a lista
+
+---TENTATIVA ERRADA (pensei que era lista como entrada)
+--sq :: ([Int] -> [Int]) -> Int
+--sq l = map (+1) l
+--sq l = foldr (+) 0 l
+
+sq :: Int -> Int
+sq n = foldr (+) 0 (map (^2) [1..n])
+
+
+
+-- Defina uma funcao que considera uma lista de inteiros como
+-- argumento e devolve a soma dos cubos dos pares
+-- Combinar map, filter e foldr
+-- filter: filtar os pares
+-- map: elevar os selecionados da lista ao cubo
+-- foldr: somar todos os elementos filtrados e elevados ao cubo
+
+sqp :: [Int] -> Int
+sqp ls = foldr (+) 0 (map (^3) (filter (even) ls))
+
+----------------------------------------------------------------
+
+
+-- Tipos definidos pelo programador
+-- Meio de declaração de tipos
+-- Ex: type String = [Char] (String eh um apelido para [Char])
+-- type Pos = (Int, Int) (Codigo mais simples - sinonimo para uma tupla de inteiros)
+-- Tipos tambem podem ter argumentos
+
+type Pos = (Int, Int)
+origem :: Pos -> Pos
+origem _ = (0,0)
+esquerda :: Pos -> Pos
+esquerda (x, y) = (x-1,y)
+
+-- type Par a = (a,a): Par eh uma tupla de elementos de um tipo a qualquer
+type Par a = (a, a)
+mult :: Par Int -> Int
+mult (m, n) = m*n
+copiar :: a -> Par a
+copiar x = (x,x)
+
+-- Declaracoes de tipos podem ser aninhadas (mas, nao recursivas)
+type Trans = Pos -> Pos
+
+
+
+-- Estruturas de Dados definidas pelo programador
+data Resposta = Yes | No | Maybe
+
+respostas :: [Resposta]
+respostas = [Yes, No, Maybe]
+
+troca :: Resposta -> Resposta
+troca Yes = No
+troca No = Yes
+troca Maybe = Maybe
+
+-- Construtores pode passar argumentos
+data Shape = Circle Float | Rectangle Float Float
+
+square :: Float -> Shape
+square n = Rectangle n n
+
+area :: Shape -> Float
+area (Circle r) = pi * r^2
+area (Rectangle x y) = x * y
